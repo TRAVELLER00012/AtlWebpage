85cdc1c9d21546f46940d296134ed24557b2facf
Daksh Kumar <k.daksh29@gmail.com>
2023-11-22 22:45:25 +0530

    Fix base url for production testing

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -2,8 +2,8 @@ import { CanceledError } from "axios";
 import axios from "axios";
 
 export default axios.create({
-    // baseURL:"https://atl-webpage-mu3u.vercel.app/api",
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://atl-webpage-mu3u.vercel.app/api",
+    // baseURL:"http://localhost:3000/api",
 
 })
 export {CanceledError}
