fe3843a7c727e6f17689164323117decb024e5f1
Daksh Kumar <k.daksh29@gmail.com>
2023-11-27 15:27:36 +0530

    FIx styling issues

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -1,7 +1,7 @@
 import { CanceledError } from "axios";
 import axios from "axios";
 export default axios.create({
-  baseURL:"https://atl-webpage-mu3u.vercel.app/api",
-    // baseURL: "http://localhost:3000/api"
+  // baseURL:"https://atl-webpage-mu3u.vercel.app/api",
+    baseURL: "http://localhost:3000/api"
 });
 export {CanceledError}  
