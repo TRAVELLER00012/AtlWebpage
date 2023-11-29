d02baf39b74109d28f548f3e2fd4117ff1b3ca8d
Daksh Kumar <k.daksh29@gmail.com>
2023-11-29 18:09:42 +0530

    Update route file for users

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -13,6 +13,19 @@ export async function GET(request:NextRequest, {params} : Props){
     const result = await prisma.user.findFirst({
         where:{
             id:parseInt(params.id)
+        },
+        select:{
+            age : true,
+            id: true,
+            firstName: true,
+            lastName : true,
+            bus_number : true,
+            number_of_years_in_atl : true,
+            phonenumber : true,
+            class : true,
+            email : true,
+            section : true,
+            user_type : true
         }
     })
     return NextResponse.json(result)
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,9 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import bcrypt from "bcrypt"
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/authOptions";
+import { TURBO_TRACE_DEFAULT_MEMORY_LIMIT } from "next/dist/shared/lib/constants";
 
 export async function GET(request:NextRequest){
-    const response = await prisma.user.findMany();
+    const response = await prisma.user.findMany({
+        select:{
+            age : true,
+            id: true,
+            firstName: true,
+            lastName : true,
+            bus_number : true,
+            number_of_years_in_atl : true,
+            phonenumber : true,
+            class : true,
+            email : true,
+            section : true,
+            user_type : true
+        }
+    });
     return NextResponse.json(response)
 }
 
